2c1ef9a47665eef4b2aa0d8577ba7c6287516c0e
NightmareFsm <nightmarefsm007@gmail.com>
2025-07-08 00:54:29 +0600

    Update build.bat

--- android/src/main/jni/build.bat
+++ android/src/main/jni/build.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem Android AAR download URL
-set ANDROID_URL=https://github.com/nightmarefsm007/ffmpeg_kit_flutter_android/releases/download/6.2/ffmpeg-kit-min-6.0-2.LTS.aar
+set ANDROID_URL=https://github.com/nightmarefsm007/ffmpeg_kit_flutter_android/releases/download/6.2/ffmpeg-kit-https-6.0-2.LTS.aar
 
 rem Create libs directory if it doesn't exist
 if not exist libs (
