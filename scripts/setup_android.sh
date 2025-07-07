c46da3f92ce77f9d55f8f258c57950d0efda804d
NightmareFsm <nightmarefsm007@gmail.com>
2025-07-08 00:55:56 +0600

    Update setup_android.sh

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Android AAR 下载
-ANDROID_URL="https://github.com/sk3llo/ffmpeg_kit_flutter/releases/download/7.0/ffmpeg-kit-full-gpl-7.0.aar"
+ANDROID_URL="https://github.com/nightmarefsm007/ffmpeg_kit_flutter_android/releases/download/6.2/ffmpeg-kit-min-6.0-2.LTS.aar"
 mkdir -p libs
 curl -L $ANDROID_URL -o libs/com.arthenica.ffmpegkit-flutter-7.0.aar
